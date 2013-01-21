b164c8d8e7987233fc3a2b6ffbfeda1a851f2bec
Jim Allman <jim@ibang.com>
2013-01-21 14:31:31 -0500

    Added "singleton" site_status table to hold a single collection of status indicators and other site-wide settings.

--- db/database-migration-004.sql
+++ db/database-migration-004.sql
@@ -17,6 +17,20 @@ CREATE TABLE publication_images (
 INSERT INTO publication_images VALUES (
   61,
   LOAD_FILE('/opt/lampp/htdocs/fossil-calibration/images/archosauria.jpeg'),
-  'I suspect this is not really an arthropod. :('
+  'OK, this is not really an arthropod. :( '
 );
  */
+
+
+/*
+ * Add a "singleton" table for site status, mostly regarding admin operations
+ */
+DROP TABLE IF EXISTS site_status;
+CREATE TABLE site_status (
+  last_build_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  needs_build TINYINT(1) DEFAULT 0,
+  build_comment VARCHAR(255),
+  last_NCBI_update_time TIMESTAMP NOT NULL,
+  announcement_title VARCHAR(255),
+  announcement_body VARCHAR(1024)
+) ENGINE=InnoDB;
