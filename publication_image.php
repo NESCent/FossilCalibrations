5b595ec92ef0b4b0634ff990c8b73ab522a8a5d0
Jim Allman <jim@ibang.com>
2013-01-21 18:26:26 -0500

    improved fetch page for publication images (detects missing query-string args)

--- publication_image.php
+++ publication_image.php
@@ -6,17 +6,20 @@ require('Site.conf');
 $connection=mysql_connect($SITEINFO['servername'],$SITEINFO['UserName'], $SITEINFO['password']) or die ('Unable to connect!');
 mysql_select_db('FossilCalibration') or die ('Unable to select database!');
 
-$id = $_GET['id'];
 
-if (!isset($id) || empty($id) || !is_numeric($id)) {
-         die("Invalid publication-image ID! ($id)");
-} else {
-
-	$query = mysql_query("SELECT * FROM publication_images WHERE PublicationID='".$id."'");
-	$row = mysql_fetch_array($query);
-	$content = $row['image'];
-
-	header('Content-type: image/jpg');
-        echo $content;
+if (!isset($_GET['id'])) {
+         die("Missing publication-image ID!");
 }
+
+$id = $_GET['id'];
+if (empty($id) || !is_numeric($id)) {
+         die("Invalid publication-image ID! ($id)");
+}
+
+$query = mysql_query("SELECT * FROM publication_images WHERE PublicationID='".$id."'");
+$row = mysql_fetch_array($query);
+$content = $row['image'];
+
+header('Content-type: image/jpg');
+echo $content;
 ?>
