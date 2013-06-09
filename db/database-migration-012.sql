3b3713db5993cc0f56c66c1769564ba4162acd1e
Jim Allman <jim@ibang.com>
2013-06-08 22:08:40 -0400

    Changes to table 'Link_CalibrationFossil' to support more age-types and an 'other' option

---
+++ db/database-migration-012.sql
@@ -0,0 +1,14 @@
+/*
+ * Adding new values for min/max date type (for fossils), and a new field to
+ * describe other types (or multiple types)
+ */
+
+INSERT INTO `L_agetypes` VALUES(3, 'paleomagnetism');
+INSERT INTO `L_agetypes` VALUES(4, 'cyclostratigraphy');
+INSERT INTO `L_agetypes` VALUES(5, 'other (please specify)');
+
+ALTER TABLE Link_CalibrationFossil
+  ADD COLUMN MinAgeTypeOtherDetails varchar(300) DEFAULT '' AFTER MinAgeType;
+
+ALTER TABLE Link_CalibrationFossil
+  ADD COLUMN MaxAgeTypeOtherDetails varchar(300) DEFAULT '' AFTER MaxAgeType;
