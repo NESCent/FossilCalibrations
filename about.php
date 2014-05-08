9c1e0ea6775a8baf910c85821e66e25927b07470
Jim Allman <jim@ibang.com>
2014-05-08 16:32:11 -0400

    Update About page with latest text, group photos.

--- about.php
+++ about.php
@@ -8,8 +8,8 @@ require('header.php');
 
 <div class="left-column">
 	<div class="link-menu" style="">
-		<a href="#fcd-mission">Mission</a>
-		<a href="#fcd-history">History</a>
+		<a href="#fcd-purpose">Purpose</a>
+		<a href="#fcd-support">Support</a>
 		<a href="#fcd-people">People</a>
 		<a href="mailto:contact@palaeontologia-electronica.org?subject=FCD%20feedback">Contact Us</a>
 	</div>
@@ -21,47 +21,54 @@ require('header.php');
 About Us
 </h1>
 
-<h3 id="fcd-mission" class="contentheading" style="margin-top: 8px; line-height: 1.25em;">Mission</h3>
+<h3 id="fcd-purpose" class="contentheading" style="margin-top: 8px; line-height: 1.25em;">Purpose</h3>
 <p>
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
+The mission of the Fossil Calibration Database is to provide vetted fossil
+calibration points that can be used for divergence dating by molecular
+systematists. These calibrations follow the five best practices outlined by
+<a href="http://sysbio.oxfordjournals.org/content/61/2/346.short" target="_blank">Parham et al. (2012)</a>.
 </p>
 
-<h3 id="fcd-history" class="contentheading" style="margin-top: 8px; line-height: 1.25em;">History</h3>
+<h3 id="fcd-support" class="contentheading" style="margin-top: 8px; line-height: 1.25em;">Support</h3>
 <p>
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
+This project was developed by the Working Group “Synthesizing and Databasing
+Fossil Calibrations: Divergence Dating and Beyond”, supported by funding from
+the National Evolutionary Synthesis Center (NSF EF-0905606). 
 </p>
 
 <h3 id="fcd-people" class="contentheading" style="margin-top: 8px; line-height: 1.25em;">People</h3>
 <p>
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
-And some useful text would appear here.
+A large collaborative team has contributed to the conception, implementation,
+and maintenance of the Fossil Calibration Database including:
+[TODO]
 </p>
+
+<img src="/images/meeting-0.jpg"/>
+<br/>
+<br/>
+<img src="/images/meeting-1.jpg"/>
+<br/>
+<br/>
+<img src="/images/meeting-2.jpg"/>
+<br/>
+<br/>
+<img src="/images/meeting-3.jpg"/>
+<br/>
+<br/>
+<img src="/images/meeting-4.jpg"/>
+
+<script type="text/javascript">
+$(document).ready(function() {
+	$('img[src*="meeting-4.jpg"]').hover(
+		function() {
+			$(this).attr('src', '/images/meeting-4a.jpg');
+		},
+		function() {
+			$(this).attr('src', '/images/meeting-4.jpg');
+		}
+	);
+});
+</script>
 
 </div><!-- END OF center-column -->
 <!--<div style="background-color: #fcc; color: #fff; clear: both;">test</div>-->
