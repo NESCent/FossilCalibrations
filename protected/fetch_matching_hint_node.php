ff2a7134b12171c9b6f13f94d1f4bde4cd044110
Jim Allman <jim@ibang.com>
2014-09-20 21:54:23 -0400

    Fix fetch of taxon source+ID from a "unique" name.

--- protected/fetch_matching_hint_node.php
+++ protected/fetch_matching_hint_node.php
@@ -22,18 +22,20 @@ mysql_select_db('FossilCalibration') or die ('Unable to select database!');
 
 // check list of names against this query
 // TODO: show un-published names only to logged-in admins/reviewers
-$query="SELECT taxonid, 'NCBI' AS source
+$query="SELECT taxonid AS nameid, 'NCBI' AS source
 	FROM NCBI_names
 	WHERE name LIKE '". mysql_real_escape_string($_POST["matched_name"]) ."'
+     OR uniquename LIKE '". mysql_real_escape_string($_POST["matched_name"]) ."'
     LIMIT 1;";
 $match_list=mysql_query($query) or die ('Error  in query: '.$query.'|'. mysql_error());	
 $node_data = mysql_fetch_assoc($match_list);
 
 if (!$node_data) {
     // fall back to FCD names *if* no NCBI node was found
-    $query="SELECT taxonid, 'FCD' AS source
+    $query="SELECT node_id AS nameid, 'FCD' AS source
         FROM FCD_names
-        WHERE name LIKE '". mysql_real_escape_string($_POST["matched_name"]) ."'".
+        WHERE name LIKE '". mysql_real_escape_string($_POST["matched_name"]) ."'
+     OR uniquename LIKE '". mysql_real_escape_string($_POST["matched_name"]) ."'".
         // non-admin users should only see *Published* publication names
         ((isset($_SESSION['IS_ADMIN_USER']) && ($_SESSION['IS_ADMIN_USER'] == true)) ? "" :  
             " AND is_public_name = 1"
@@ -49,4 +51,4 @@ if (!$node_data) {
        name="hintNodeSource_<?= $_POST["side"] ?>[]" id="hintNodeSource_<?= $_POST["side"] ?>_<?= $_POST["position"] ?>" value="<?= (!$node_data) ? 'ERROR' : $node_data['source'] ?>" />
 
 <input type="text" readonly="readonly" style="width: 15%; color: #999; text-align: center;" 
-       name="hintNodeID_<?= $_POST["side"] ?>[]" id="hintNodeID_<?= $_POST["side"] ?>_<?= $_POST["position"] ?>" value="<?= (!$node_data) ? 'ERROR' : $node_data['taxonid'] ?>" />
+       name="hintNodeID_<?= $_POST["side"] ?>[]" id="hintNodeID_<?= $_POST["side"] ?>_<?= $_POST["position"] ?>" value="<?= (!$node_data) ? 'ERROR' : $node_data['nameid'] ?>" />
